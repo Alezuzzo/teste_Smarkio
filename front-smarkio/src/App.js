b0696d37c1e3554b03a9e9b46ee7abdc5a16c043
Thalles Alexsander <thallesander@hotmail.com>
2021-07-31 16:46:20 -0300

    adicionando front e estilos

---
+++ front-smarkio/src/App.js
@@ -0,0 +1,81 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="container">
+      <div class = "commentArea">
+        <span>Comentários</span>
+        <textarea cols="30" rows="10"></textarea>
+        <button class = "buttonCad">Cadastrar</button>
+      </div>
+      <div class="line"></div>
+      <div class="allComments">
+        <span>Comentários</span>
+        <div>
+          <div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div>
+          <div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div>
+          <div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div>
+          <div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div><div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div><div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div><div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div><div class = "card">
+            <p class = "comments" >
+              lorem ipsum dolor sit amet consectetur atur atur atur atur atur
+              atur a lorem ipsum dolor sit amet consectetur atur a lorem ipsum
+              dolor sit amet consectetur a lorem ipsum dolor sit.
+            </p>
+            <button class="buttonListen">Ouvir</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- teste2-watson_back/index.js
+++ teste2-watson_back/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const connectionDb = require('./db')
 const app = express()
-const port = 3000
+const port = 3333
 
 
 connectionDb();
